3aa2fc29464097a1428bba8f30d4c23b4df6f751
Aafreen <aafreen2912@gmail.com>
2026-03-05 21:04:09 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+username=input("Create a username")
+password=input("Create a password")
+
+print("ACCOUNT CREATED")
+ID=input("Enter your username")
+PASS=input("Enter your password")
+
+if ID==username and PASS==password:
+  print("Successful 🌸")
+else:
+  print("UGH 🖕")
